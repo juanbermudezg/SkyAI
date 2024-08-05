7225cbe2987c11b423c29ac696233a0ae514ee83
Juan Bermudez <juanbermudez_10@ieee.org>
2024-08-04 22:35:29 -0500

    delete first ai

--- sky/utils/functionAI.py
+++ sky/utils/functionAI.py
@@ -1,11 +1,7 @@
 from datetime import date, time
-from .first_ai import loadAirports, cargarModelo, prepare_input
 from .sencond_ai import verificar_fecha_hora, new_Dataframe, make_prediction, adjust_time
 
 def finalDef(dictFligth):
-    allAirports = loadAirports()
-    loaded_model_final = cargarModelo()
-    expected_columns = loaded_model_final.feature_names_in_
     year = dictFligth.get('year')
     month = dictFligth.get('month')
     day = dictFligth.get('day')
@@ -15,14 +11,8 @@ def finalDef(dictFligth):
     minuteSTA = dictFligth.get('minuteSTA')
     from_location = dictFligth.get('from_location')
     to_location = dictFligth.get('to_location')
-    airliner = dictFligth.get('airliner')
     airlinerName = dictFligth.get('airlinerName')
-    input_data = prepare_input(year, month, day, hourSTD, minuteSTD, hourSTA, minuteSTA, airliner, from_location, to_location, allAirports)
-    for col in expected_columns:
-        if col not in input_data:
-            input_data[col] = False
-    input_data = input_data[expected_columns]
-    predictions = loaded_model_final.predict(input_data)
+    predictions = ['Normal']
     status_text = predictions[0]
     airportsCompleted = ['BAQ', 'BOG', 'CLO', 'CTG', 'MDE', 'MIA', 'SMR']
     message = "Datos calculados exitosamente."
